4e0ea9b994278805be77fb4baea6c00a2a8b860d
karmab <karimboumedhel@gmail.com>
2013-04-08 18:12:48 +0200

    only display default client when there s one

--- foreman.py
+++ foreman.py
@@ -14,6 +14,7 @@ __email__ = "karimboumedhel@gmail.com"
 __status__ = "Production"
 
 import ast
+import os
 import sys
 import re
 import pexpect
@@ -23,6 +24,10 @@ import pycurl
 import StringIO
 import optparse
 import urllib
+import ConfigParser
+
+
+ERR_NOFOREMANFILE="You need to create a correct foreman.ini file in your home directory.Check documentation"
 
 def foremando(url,actiontype=None,postdata=None):
  if postdata:postdata="%s" % str(postdata).replace("'",'"')
@@ -76,8 +81,8 @@ def foremancreate(url,name,ip=None,osid=None,envid=None,archid=None,puppetid=Non
  postdata="%s" % str(postdata).replace("'",'"')
  foremando(url,actiontype="POST",postdata=postdata)
  
-def foremangetid(foreman,searchtype,searchname):
- url="http://%s/api/%s/%s" % (foreman,searchtype,searchname)
+def foremangetid(foremanhost,searchtype,searchname):
+ url="http://%s/api/%s/%s" % (foremanhost,searchtype,searchname)
  result=foremando(url)
  if searchtype.endswith("es"):
   shortname=searchtype[:-2]
@@ -118,12 +123,18 @@ parser.add_option("-m", "--mac", dest="mac", type="string",help="Mac to use when
 parser.add_option("-M", "--memory", dest="memory", type="int", default=2048, help="Amount of memory to use when creating new server")
 parser.add_option("-2", "--cpus", dest="cores", type="int", default=2, help="Numbers of cpus to use when creating new server.Defaults to 1")
 parser.add_option("-p", "--powerup", dest="powerup", type="int", default=0, help="Whether to powerup server after creation.Defaults to no")
-parser.add_option("-f", "--foreman", dest="foreman", type="string",help="Foreman server or ip to use.Currently default to puppet host if not specified")
+parser.add_option("-f", "--host", dest="foremanhost", type="string",help="Foreman server or ip to use.Currently default to puppet host if not specified")
 parser.add_option("-g", "--group", dest="hostgroup", type="string",help="Hostgroup to use when creating new server ")
 parser.add_option("-c", "--computeresource", dest="compute_resource_id", type="int",help="Computeresource_id to use when creating new server")
 parser.add_option("-u", "--update", dest="update", action="store_true",help="Update existing server")
 parser.add_option("-v", "--verbose", dest="verbose", action="store_true",help="Verbose mode")
 parser.add_option("-7", "--puppetclass", dest="puppetclass", type="string",help="Add given class to a host")
+listinggroup = optparse.OptionGroup(parser, "Listing options")
+#listinggroup.add_option("-l", "--listprofiles", dest="listprofiles", action="store_true", help="list available profiles")
+#listinggroup.add_option("-H", "--listhosts", dest="listhosts", action="store_true", help="List hosts")
+listinggroup.add_option("-L", "--listclients", dest="listclients", action="store_true", help="list available clients")
+listinggroup.add_option("-9", "--switchclient", dest="switchclient", type="string", help="Switch default client")
+parser.add_option_group(listinggroup)
 
 (options, args) = parser.parse_args()
 environments=options.environments
@@ -136,7 +147,7 @@ domains=options.domains
 status=options.status
 dashboard=options.dashboard
 errors=options.errors
-foreman=options.foreman
+foremanhost=options.foremanhost
 hostgroup=options.hostgroup
 powerup=options.powerup
 ip=options.ip
@@ -150,8 +161,93 @@ puppet=options.puppet
 update=options.update
 verbose=options.verbose
 puppetclass=options.puppetclass
+listclients = options.listclients
+switchclient = options.switchclient
 
-if not foreman:foreman="puppet"
+
+foremanconffile="%s/foreman.ini" %(os.environ['HOME'])
+#parse foreman client auth file
+if not os.path.exists(foremanconffile):
+ print "Missing %s in your  home directory.Check documentation" % foremanconffile
+ sys.exit(1)
+try:
+ c = ConfigParser.ConfigParser()
+ c.read(foremanconffile)
+ foremans={}
+ default={}
+ for cli in c.sections():
+  for option in  c.options(cli):
+   if cli=="default":
+    default[option]=c.get(cli,option)
+    continue
+   if not foremans.has_key(cli):
+    foremans[cli]={option : c.get(cli,option)}
+   else:
+    foremans[cli][option]=c.get(cli,option)
+except KeyError,e:
+ print ERR_NOFOREMANFILE
+ print e
+ os._exit(1)
+
+if listclients:
+ print "Available Clients:"
+ for cli in  sorted(foremans):
+  print cli
+ if default.has_key("client"):print "Current default client is: %s" % (default["client"])
+ sys.exit(0)
+
+if switchclient:
+ if switchclient not in foremans.keys():
+  print "Client not defined...Leaving"
+ else:
+  mod = open(foremanconffile).readlines()
+  f=open(foremanconffile,"w")
+  for line in mod:
+   if line.startswith("client"):
+    f.write("client=%s\n" % switchclient)
+   else:
+    f.write(line)
+  f.close()
+  print "Default Client set to %s" % (switchclient)
+ sys.exit(0)
+
+if not client:
+ try:
+  client=default['client']
+ except:
+  print "No client defined as default in your ini file or specified in command line"
+  os._exit(1)
+
+#PARSE DEFAULT SECTION
+try:
+ if not host and default.has_key("clu"):clu=default["clu"]
+ if not numcpu and default.has_key("numcpu"):numcpu=int(default["numcpu"])
+ if not diskformat and default.has_key("diskformat"):diskformat=default["diskformat"]
+ if default.has_key("disksize"):disksize=int(default["disksize"])*GB
+ if default.has_key("memory"):memory=int(default["memory"])*MB
+ if default.has_key("low"):low=float(default["low"])
+ if not storagedomain and default.has_key("storagedomain"):storagedomain=default["storagedomain"]
+ if not numinterfaces and default.has_key("numinterfaces"):numinterfaces=int(default["numinterfaces"])
+ if not ossl and default.has_key("ssl"):ossl=True
+except:
+ print "Problem parsing default section in your ini file"
+ os._exit(1)
+
+try:
+ foremanhost=foremans[client]["host"]
+ if foremans[client].has_key("port"):foremanport=foremans[client]["port"]
+ if foremans[client].has_key("user"):foremanuser=foremans[client]["user"]
+ if foremans[client].has_key("password"):foremanpassword=foremans[client]["password"]
+ if foremans[client].has_key("mac"):mac=foremans[client]["mac"]
+ if foremans[client].has_key("os"):foremanos=foremans[client]["os"]
+ if foremans[client].has_key("env"):foremanenv=foremans[client]["env"]
+ if foremans[client].has_key("arch"):foremanarch=foremans[client]["env"]
+ if foremans[client].has_key("puppet"):foremanpuppet=foremans[client]["puppet"]
+ if foremans[client].has_key("org"):foremanptable=foremans[client]["ptable"]
+ if not dns and foremans[client].has_key('dns'):dns=foremans[client]['dns']
+except KeyError,e:
+ print "Problem parsing foreman ini file:Missing parameter %s" % e
+ os._exit(1)
 
 #url="http://puppet/hosts"
 if not hosts and not hostgroups and not name and not archs and not domains and not status and not dashboard and not errors and not compute_resources and not operatingsystems and not environments and not puppet and not kill and not update  and not puppetclass:
@@ -159,32 +255,32 @@ if not hosts and not hostgroups and not name and not archs and not domains and n
  sys.exit(1)
 
 if hosts or name:
- url="http://%s/api/hosts" % (foreman)
+ url="http://%s/api/hosts" % (foremanhost)
 if hostgroups:
- url="http://%s/api/hostgroups"  % (foreman)
+ url="http://%s/api/hostgroups"  % (foremanhost)
 if archs:
- url="http://%s/api/architectures"  % (foreman)
+ url="http://%s/api/architectures"  % (foremanhost)
 if domains:
- url="http://%s/api/domains"  % (foreman)
+ url="http://%s/api/domains"  % (foremanhost)
 if status:
- url="http://%s/api/status"  % (foreman)
+ url="http://%s/api/status"  % (foremanhost)
 if dashboard:
- url="http://%s/api/dashboard"  % (foreman)
+ url="http://%s/api/dashboard"  % (foremanhost)
 if errors:
- url="http://%s/api/hosts/errors"  % (foreman)
+ url="http://%s/api/hosts/errors"  % (foremanhost)
 if compute_resources:
- url="http://%s/api/compute_resources"  % (foreman)
+ url="http://%s/api/compute_resources"  % (foremanhost)
 if operatingsystems:
- url="http://%s/api/operatingsystems"  % (foreman)
+ url="http://%s/api/operatingsystems"  % (foremanhost)
 if environments:
- url="http://%s/api/environments"  % (foreman)
+ url="http://%s/api/environments"  % (foremanhost)
 if puppet:
- url="http://%s/api/smart_proxies?type=puppet"  % (foreman)
- #url="http://%s/ptables"  % (foreman)
+ url="http://%s/api/smart_proxies?type=puppet"  % (foremanhost)
+ #url="http://%s/ptables"  % (foremanhost)
 
 
 if kill:
- url="http://%s/hosts/%s" % (foreman,kill)
+ url="http://%s/hosts/%s" % (foremanhost,kill)
  foremando(url,actiontype="DELETE")
  sys.exit(0)
 
@@ -194,7 +290,7 @@ if ip and update:
  if name=="":
   print "name cant be blank"
   sys.exit(1)
- url="http://%s/api/hosts/%s" % (foreman,name)
+ url="http://%s/api/hosts/%s" % (foremanhost,name)
  foremanupdateip(url,name,ip)
  sys.exit(0)
 
